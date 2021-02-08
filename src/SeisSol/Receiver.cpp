475632727458989279ab053ff51ddb30f90e3409
Sebastian Wolf <wolf.sebastian@in.tum.de>
2021-02-08 11:44:23 +0100

    Also use operator() for this argument in Receiver::l1Difference

--- src/SeisSol/Receiver.cpp
+++ src/SeisSol/Receiver.cpp
@@ -9,7 +9,8 @@ double SeisSol::Receiver::l1Difference(const Receiver& otherReceiver) const {
     for (size_t j = 1; j < 10; j++) {
       //TODO think of something better here
       //proably we need time interpolation once we have data read from SeisSol output. We also need to weight different quantities (stresses are ~1e9, velocities ~1e0)
-      l += std::abs(receiverData[i][j] - otherReceiver(i,j));
+      //l += std::abs(receiverData[i][j] - otherReceiver(i,j));
+      l += std::abs(this->operator()(i,j) - otherReceiver(i,j));
     }
   }
   
