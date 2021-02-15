9921fee35678af6fa858040d3a47d9fa7d66ce60
Radu Raicea <radu.raicea@tum.de>
2021-02-15 11:42:05 +0100

    Run SeisSol with parameters file

--- src/SeisSol/Runner.h
+++ src/SeisSol/Runner.h
@@ -6,11 +6,12 @@ namespace SeisSol {
 
   class Runner {
     public:
-      Runner(std::string path);
+      Runner(std::string seisSolBinaryPath, std::string parametersFilePath);
       int run() const;
     
     private:
       std::string binaryPath;
+      std::string parametersPath;
 
   };
 
