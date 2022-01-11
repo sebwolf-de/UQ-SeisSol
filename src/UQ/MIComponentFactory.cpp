b4092d4a34798c25971aa2303795bdafccb2cd8d
Nils Sperling <ga63goh@mytum.de>
2022-01-11 11:28:30 +0100

    Added TODO: change how to calc cov

--- src/UQ/MIComponentFactory.cpp
+++ src/UQ/MIComponentFactory.cpp
@@ -18,6 +18,7 @@ std::shared_ptr<UQ::MCMCProposal> UQ::MyMIComponentFactory::Proposal(
   auto mu = Eigen::VectorXd::Zero(numberOfParameters);
   Eigen::MatrixXd cov = Eigen::MatrixXd::Identity(numberOfParameters, numberOfParameters);
   
+  // TODO: find alternative way to define cov since startingParameters might be orders of magnitude apart
   for (size_t i=0; i < numberOfParameters; i++) {
     cov(i, i) = cov(i, i) * std::pow(startingParameters(i), 2) / 100;
   }
